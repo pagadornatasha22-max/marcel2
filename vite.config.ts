72e880d874e64df8f7be3850f981ff40432aa1ec
pagadornatasha22-max <pagadornatasha22@gmail.com>
2026-05-18 23:18:21 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,13 @@ const __dirname = path.dirname(__filename);
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), viteSingleFile()],
+
+  preview: {
+    allowedHosts: [
+      "marcel2.onrender.com"
+    ],
+  },
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
